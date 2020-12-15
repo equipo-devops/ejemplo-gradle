092189f2438b4649b0a13ad0a584baa1d4ba3e4b
Esteban Erices Peña <esteban.erices@gmail.com>
2020-12-15 20:14:57 -0300

    configuracion maven gradle

--- gradle.groovy
+++ gradle.groovy
@@ -9,9 +9,50 @@
 
 def call(){
   
-  stage(){
+
     //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
-  }
+     
+                script {
+                    stage('build & test') {
+                          sh  "gradle clean build"
+                    }
+                    stage('sonar') {
+                           def scannerHome = tool 'sonar-scanner'; // scanner
+                        withSonarQubeEnv('Sonar') { // server
+                            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build " 
+                        }
+                    
+                    }
+                    stage('run') {
+
+                         sh "nohup bash gradle bootRun &"
+                        
+                    }
+                      stage('rest') {
+                        sleep(time: 10, unit: 'SECONDS')
+                        sh 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
+                    }
+                    stage('nexus') {
+                    	    nexusArtifactUploader(
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        nexusUrl: 'localhost:8081',
+                        groupId: 'com.devopsusach2020',
+                        version: '0.0.1',
+                        repository: 'test-nexus',
+                        credentialsId: 'nexus',
+                        artifacts: [
+                            [artifactId: 'DevOpsUsach2020',
+                            classifier: '',
+                            file: 'build/libs/DevOpsUsach2020-0.0.1.jar',
+                            type: 'jar']
+                        ]
+                        )
+                        
+                    }
+                }
+            
+  
 
 }
 
--- maven.groovy
+++ maven.groovy
@@ -9,8 +9,61 @@
 
 def call(){
   
-  stage(){
+  script{
     //Escribir directamente el código del stage, sin agregarle otra clausula de Jenkins.
+            stage('Compile') {
+          
+              sh './mvnw clean compile -e'
+          
+            
+        }
+        stage('Unit') {
+            
+             
+                    sh './mvnw clean test -e'
+            
+            
+        }
+        stage('Jar') {
+          
+                  
+                    sh './mvnw clean package -e'
+            
+            
+        }
+
+          stage('SonarQube') {
+          	
+    			withSonarQubeEnv('Sonar') { // You can override the credential to be used
+      			sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+    				}
+			   
+  		}
+
+        stage('Run') {
+            
+            
+                    sh 'nohup bash mvnw spring-boot:run &'
+                    
+              
+             
+               
+            
+        }
+         stage('Test') {
+            
+                sleep 20
+                sh 'curl http://localhost:8081/rest/mscovid/test?msg=testing'
+            
+        } 
+        
+       stage('Stop') {
+            
+                    sh 'bash mvnw spring-boot:stop &'
+                    
+                 
+            
+        }
   }
 
 }
